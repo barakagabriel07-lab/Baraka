2ba485f000996490c57324f2bdd3fd3dcf6a49e0
barakagabriel07-lab <barakagabriel07@gmail.com>
2026-06-29 10:01:46 +0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2w3Vds-YZ2miChbRTcfOoizoDs0cHt6g",
+  authDomain: "bg-connect-ba650.firebaseapp.com",
+  projectId: "bg-connect-ba650",
+  storageBucket: "bg-connect-ba650.firebasestorage.app",
+  messagingSenderId: "454995749393",
+  appId: "1:454995749393:web:23f1f21b4719c38ce8dfb0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
